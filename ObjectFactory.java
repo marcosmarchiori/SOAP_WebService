19ffb4a08b8a1e3a5ccef6320789ef5e05fde7a0
Marcos Marchiori <marcos_marchiori@intranet.itresources>
2019-12-27 16:42:04 -0300

    consuming web service

---
+++ ObjectFactory.java
@@ -0,0 +1,59 @@
+
+package com.example.consumingwebservice;
+import javax.xml.bind.annotation.XmlRegistry;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.example.demo.wsdl package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.example.demo.wsdl
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCountryRequest }
+     * 
+     */
+    public GetCountryRequest createGetCountryRequest() {
+        return new GetCountryRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetCountryResponse }
+     * 
+     */
+    public GetCountryResponse createGetCountryResponse() {
+        return new GetCountryResponse();
+    }
+
+    /**
+     * Create an instance of {@link Country }
+     * 
+     */
+    public Country createCountry() {
+        return new Country();
+    }
+
+}
